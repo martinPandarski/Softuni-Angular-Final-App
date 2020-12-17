a5f8e63a62a62a69c1200b24b5098c9dbda97db5
martinPandarski <marto65481@gmail.com>
2020-12-17 22:02:22 +0200

    Fix for Heroku deployment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   apiUrl: `https://api.backendless.com/A0A4A48D-7CE3-F038-FFE0-C65DD01FF200/81333D2F-F23B-412B-915A-01428F24C2C6`
 };
 
